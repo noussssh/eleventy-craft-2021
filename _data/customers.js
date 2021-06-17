39ddc88d38aced32bf08bdb2f4f04af82b687e1a
noussh <noussh@nousshs-MacBook-Pro.local>
2021-06-17 12:55:03 +0400

    customers list on homepage

---
+++ _data/customers.js
@@ -0,0 +1,138 @@
+module.exports = [
+    {
+      "name": "Palfinger",
+      "logo": "palfinger.png",    
+    },    
+    {
+      "name": "BROOK INTERNATIONAL CONTRACTING & TRADING EST.",
+      "logo": "brook.png",    
+    },    
+    {
+      "name": "Dubai leading technologies",
+      "logo": "dubai-leading.png",    
+    },    
+    {
+      "name": "Manzil Healthcare",
+      "logo": "manzil.png",    
+    },    
+    {
+      "name": "Al Safwan Marine",
+      "logo": "alsafwan.png",    
+    },    
+    {
+        "name": "Unimax Pharma",
+        "logo": "unimax.jpeg",    
+    }, 
+    {
+      "name": "Genfocus",
+      "logo": "genfocus.png",    
+    },    
+    {
+      "name": "Computer care group",
+      "logo": "computer-care.jpeg",    
+    },    
+    {
+      "name": "European united company LLC",
+      "logo": "euc.gif",    
+    },    
+    {
+      "name": "I Vision holdings",
+      "logo": "ivision.png",    
+    },    
+    {
+      "name": "Mid Asia bulk",
+      "logo": "mid-asia-bulk.png",    
+    },    
+    {
+      "name": "Emdad technical services",
+      "logo": "ets.png",    
+    },    
+    {
+      "name": "Global Hardware and Tools LLC",
+      "logo": "global-hardware.png",    
+    },    
+    {
+      "name": "AQL Groups",
+      "logo": "aql.jpeg",    
+    },    
+    {
+      "name": "Resolute Group",
+      "logo": "resolute.png",    
+    },    
+    {
+      "name": "Office One",
+      "logo": "office-one.png",    
+    },    
+    {
+      "name": "Al Murjan Group",
+      "logo": "al-murjan.png",    
+    },    
+    {
+      "name": "Betomatic Middle East",
+      "logo": "betomatic.jpeg",    
+    },    
+    {
+      "name": "Inoquest LABS",
+      "logo": "inoquest.png",    
+    },    
+    {
+      "name": "Wahat Al Aman Home Healthcare Services",
+      "logo": "wahat.webp",    
+    },    
+    {
+      "name": "Speedblast Trading LLC",
+      "logo": "speedblast.png",    
+    },    
+    {
+      "name": "Candid Group of companies",
+      "logo": "candid.png",    
+    },    
+    {
+      "name": "Seven Layers Information Technology",
+      "logo": "seven-layers.jpeg",    
+    },       
+    {
+      "name": "Ahuja Electrical Appliances Co. LLC",
+      "logo": "ahuja.png",    
+    },    
+    {
+      "name": "SDC International",
+      "logo": "sdc.png",    
+    },    
+    {
+      "name": "Armoplast Plastics Industries LLC",
+      "logo": "armoplast.png",    
+    },    
+    {
+      "name": "Newcom Computers System LLC",
+      "logo": "newcom.jpeg",    
+    },    
+    {
+      "name": "Yoola Security & Cleaning Services",
+      "logo": "yoola.png",    
+    },    
+    {
+      "name": "Rosenbauer for Vehicles Maintenance LLC",
+      "logo": "rosenbaur.png",    
+    },    
+    {
+      "name": "Saif Al Burhan Traders LLC",
+      "logo": "saif-al-burhan.png",    
+    },    
+    {
+      "name": "Petropipe",
+      "logo": "petropipe.jpeg",    
+    },    
+    {
+      "name": "Yuttah FZE",
+      "logo": "yuttah.png",    
+    },      
+    {
+      "name": "Ain Al Shams Workshop Equipment & Tools Trading Tr.LLC",
+      "logo": "ain-al-shams.png",    
+    },    
+    {
+      "name": "Louis Safety",
+      "logo": "louis-safety.png",    
+    },       
+  ]
