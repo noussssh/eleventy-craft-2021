fa841cfb1b35ad7b75fac2a50f7c0641b09bc07e
Noushad Moidunny <67584719+noussssh@users.noreply.github.com>
2022-02-12 20:01:02 +0400

    clients page added

--- _data/customers.js
+++ _data/customers.js
@@ -1,138 +1,248 @@
 module.exports = [
-    {
-      "name": "Palfinger",
-      "logo": "palfinger.png",    
-    },    
-    {
-      "name": "BROOK INTERNATIONAL CONTRACTING & TRADING EST.",
-      "logo": "brook.png",    
-    },    
-    {
-      "name": "Dubai leading technologies",
-      "logo": "dubai-leading.png",    
-    },    
-    {
-      "name": "Manzil Healthcare",
-      "logo": "manzil.png",    
-    },    
-    {
-      "name": "Al Safwan Marine",
-      "logo": "alsafwan.png",    
-    },    
-    {
-        "name": "Unimax Pharma",
-        "logo": "unimax.jpeg",    
-    }, 
-    {
-      "name": "Genfocus",
-      "logo": "genfocus.png",    
-    },    
-    {
-      "name": "Computer care group",
-      "logo": "computer-care.jpeg",    
-    },    
-    {
-      "name": "European united company LLC",
-      "logo": "euc.gif",    
-    },    
-    {
-      "name": "I Vision holdings",
-      "logo": "ivision.png",    
-    },    
-    {
-      "name": "Mid Asia bulk",
-      "logo": "mid-asia-bulk.png",    
-    },    
-    {
-      "name": "Emdad technical services",
-      "logo": "ets.png",    
-    },    
-    {
-      "name": "Global Hardware and Tools LLC",
-      "logo": "global-hardware.png",    
-    },    
-    {
-      "name": "AQL Groups",
-      "logo": "aql.jpeg",    
-    },    
-    {
-      "name": "Resolute Group",
-      "logo": "resolute.png",    
-    },    
-    {
-      "name": "Office One",
-      "logo": "office-one.png",    
-    },    
-    {
-      "name": "Al Murjan Group",
-      "logo": "al-murjan.png",    
-    },    
-    {
-      "name": "Betomatic Middle East",
-      "logo": "betomatic.jpeg",    
-    },    
-    {
-      "name": "Inoquest LABS",
-      "logo": "inoquest.png",    
-    },    
-    {
-      "name": "Wahat Al Aman Home Healthcare Services",
-      "logo": "wahat.webp",    
-    },    
-    {
-      "name": "Speedblast Trading LLC",
-      "logo": "speedblast.png",    
-    },    
-    {
-      "name": "Candid Group of companies",
-      "logo": "candid.png",    
-    },    
-    {
-      "name": "Seven Layers Information Technology",
-      "logo": "seven-layers.jpeg",    
-    },       
-    {
-      "name": "Ahuja Electrical Appliances Co. LLC",
-      "logo": "ahuja.png",    
-    },    
-    {
-      "name": "SDC International",
-      "logo": "sdc.png",    
-    },    
-    {
-      "name": "Armoplast Plastics Industries LLC",
-      "logo": "armoplast.png",    
-    },    
-    {
-      "name": "Newcom Computers System LLC",
-      "logo": "newcom.jpeg",    
-    },    
-    {
-      "name": "Yoola Security & Cleaning Services",
-      "logo": "yoola.png",    
-    },    
-    {
-      "name": "Rosenbauer for Vehicles Maintenance LLC",
-      "logo": "rosenbaur.png",    
-    },    
-    {
-      "name": "Saif Al Burhan Traders LLC",
-      "logo": "saif-al-burhan.png",    
-    },    
-    {
-      "name": "Petropipe",
-      "logo": "petropipe.jpeg",    
-    },    
-    {
-      "name": "Yuttah FZE",
-      "logo": "yuttah.png",    
-    },      
-    {
-      "name": "Ain Al Shams Workshop Equipment & Tools Trading Tr.LLC",
-      "logo": "ain-al-shams.png",    
-    },    
-    {
-      "name": "Louis Safety",
-      "logo": "louis-safety.png",    
-    },       
-  ]
+  {
+    name: "Palfinger",
+    logo: "palfinger.png",
+    home: "True",
+  },
+  {
+    name: "BROOK INTERNATIONAL CONTRACTING & TRADING EST.",
+    logo: "brook.png",
+    home: "True",
+  },
+  {
+    name: "Dubai leading technologies",
+    logo: "dubai-leading.png",
+    home: "True",
+  },
+  {
+    name: "Manzil Healthcare",
+    logo: "manzil.png",
+    home: "True",
+  },
+  {
+    name: "Al Safwan Marine",
+    logo: "alsafwan.png",
+    home: "True",
+  },
+  {
+    name: "Unimax Pharma",
+    logo: "unimax.jpeg",
+    home: "True",
+  },
+  {
+    name: "Genfocus",
+    logo: "genfocus.png",
+  },
+  {
+    name: "Computer care group",
+    logo: "computer-care.jpeg",
+  },
+  {
+    name: "European united company LLC",
+    logo: "euc.gif",
+  },
+  {
+    name: "I Vision holdings",
+    logo: "ivision.png",
+  },
+  {
+    name: "Mid Asia bulk",
+    logo: "mid-asia-bulk.png",
+  },
+  {
+    name: "Emdad technical services",
+    logo: "ets.png",
+  },
+  {
+    name: "Global Hardware and Tools LLC",
+    logo: "global-hardware.png",
+  },
+  {
+    name: "AQL Groups",
+    logo: "aql.jpeg",
+  },
+  {
+    name: "Resolute Group",
+    logo: "resolute.png",
+  },
+  {
+    name: "Office One",
+    logo: "office-one.png",
+  },
+  {
+    name: "Al Murjan Group",
+    logo: "al-murjan.png",
+  },
+  {
+    name: "Betomatic Middle East",
+    logo: "betomatic.jpeg",
+  },
+  {
+    name: "Inoquest LABS",
+    logo: "inoquest.png",
+  },
+  {
+    name: "Wahat Al Aman Home Healthcare Services",
+    logo: "wahat.webp",
+  },
+  {
+    name: "Speedblast Trading LLC",
+    logo: "speedblast.png",
+  },
+  {
+    name: "Candid Group of companies",
+    logo: "candid.png",
+    home: "True",
+  },
+  {
+    name: "Seven Layers Information Technology",
+    logo: "seven-layers.jpeg",
+  },
+  {
+    name: "Ahuja Electrical Appliances Co. LLC",
+    logo: "ahuja.png",
+  },
+  {
+    name: "SDC International",
+    logo: "sdc.png",
+  },
+  {
+    name: "Armoplast Plastics Industries LLC",
+    logo: "armoplast.png",
+  },
+  {
+    name: "Newcom Computers System LLC",
+    logo: "newcom.jpeg",
+  },
+  {
+    name: "Yoola Security & Cleaning Services",
+    logo: "yoola.png",
+  },
+  {
+    name: "Rosenbauer for Vehicles Maintenance LLC",
+    logo: "rosenbaur.png",
+    home: "True",
+  },
+  {
+    name: "Saif Al Burhan Traders LLC",
+    logo: "saif-al-burhan.png",
+  },
+  {
+    name: "Petropipe",
+    logo: "petropipe.jpeg",
+  },
+  {
+    name: "Yuttah FZE",
+    logo: "yuttah.png",
+  },
+  {
+    name: "Ain Al Shams Workshop Equipment & Tools Trading Tr.LLC",
+    logo: "ain-al-shams.png",
+  },
+  {
+    name: "Louis Safety",
+    logo: "louis-safety.png",
+    home: "True",
+  },
+  {
+    name: "Worood Intraflora LLC",
+    logo: "Worood.png",
+    home: "True",
+  },
+  {
+    name: "Emirati Coffee Roastery",
+    logo: "emirati-coffee-roastery.png",
+    home: "True",
+  },
+  {
+    name: "Safinat Al Sahraa",
+    logo: "Safinat.png",
+  },
+  {
+    name: "VERTECO Turnkey Projects LLC",
+    logo: "VERTECO.png",
+  },
+  {
+    name: "Sulfah",
+    logo: "sulfa.png",
+    home: "True",
+  },
+  {
+    name: "Experts Team Building Maintainance UAE",
+    logo: "erperts-team.png",
+  },
+  {
+    name: "Liwa Speciality Roastery",
+    logo: "Liwa-Speciality-Roastery.png",
+    home: "True",
+  },
+  {
+    name: "Oil and Gas International FZC",
+    logo: "ogi.png",
+  },
+  {
+    name: "RELY Industries FZCO",
+    logo: "rely-industries.png",
+    home: "True",
+  },
+  {
+    name: "Royal Pharmaceutical Drug Store LLC",
+    logo: "royal-pharma.jpeg",
+  },
+  {
+    name: "Morpho Flowers",
+    logo: "morpho.jpeg",
+    home: "True",
+  },
+  {
+    name: "DTA Maritime L.L.C.",
+    logo: "DTA-maritime.jpeg",
+    home: "True",
+  },
+  {
+    name: "Dubai Engineering CO. LLC",
+    logo: "DECO-Logo.png",
+  },
+  {
+    name: "Captain Engineering Turning Co LLC",
+    logo: "captain.png",
+  },
+  {
+    name: "Orill Oilfeild Supplies LLC",
+    logo: "orill.jpeg",
+  },
+  {
+    name: "Active Fitness Store LLC",
+    logo: "active-fitness-store.png",
+  },
+  {
+    name: "Core General Trading",
+    logo: "core.png",
+  },
+  {
+    name: "Seven Capital Trading LLC",
+    logo: "seven-capitals.jpeg",
+  },
+  {
+    name: "Afritrac Automotive FZCO",
+    logo: "Afritrac.png",
+  },
+  {
+    name: "NIZMET Trading FZE",
+    logo: "gold-nizmet-logo.png",
+  },
+  {
+    name: "Al Yanbou Electro Mechanical Works LLC",
+    logo: "al-yanbou.jpeg",
+  },
+  {
+    name: "Cosma care cosmetics manufacturing",
+    logo: "cosmacare.png",
+    home: "True",
+  },
+  {
+    name: "Bulandi Distributors FZCO",
+    logo: "bulandi.png",
+    home: "True",
+  },
+];
